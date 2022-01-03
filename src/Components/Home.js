cb960e9c3a514dce5c6b05ed2680c57328266672
Tarun Singh Yadav <tarunyadav148@gmail.com>
2022-01-03 18:23:56 +0530

    Made landing of homepage responsive

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
+import "./styles/Home.css"
 
 function Home() {
   return (
-    <div>
-      {/* <div className="header-container">
-        <div className="wrapper row"> */}
-      <header className="row">
-        <div className="hero-content col col-md-6">
+      <div className="landing">
+
+        <div className="landindContent">
+          
           <h1>
             OpenSource meets its{" "}
             <span className="dynamic-text">Contributor Force</span>
@@ -14,21 +14,24 @@ function Home() {
           <p>
             10th to 15th Jan <span className="span-color">2021</span>
           </p>
-          <button className="register">Register</button>
-          <button className="discord">
-            <img src="./images/Discord.png" alt="" />
-            <a href="https://discord.com/invite/s77kYnfSGf" target={"_blank"} >Discord</a>
-          </button>
+
+          <div className="landingBtn--wapper">
+            <button className="register landingBtn">Register</button>
+            <button className="discord landingBtn">
+              <img src="./images/Discord.png" alt="" />
+              <a href="https://discord.com/invite/s77kYnfSGf" target={"_blank"} >Discord</a>
+            </button>
+          </div>
+
         </div>
-        <div className="hero-image col col-md-6">
+
+        <div className="landingImage">
             <img src="./images/Main-image.png" />
 
           <div className="photo-bg"></div>
         </div>
-      </header>
-      {/* </div>
-      </div> */}
-    </div>
+
+      </div>
   );
 }
 export default Home;
