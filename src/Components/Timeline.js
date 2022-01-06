59f9f07729ddb35861c9ffca7cf073706a4d382a
Deepak-Kumar201 <dk10112002@gmail.com>
2022-01-06 10:31:46 +0530

    timeline line added

--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -1,15 +1,45 @@
 import React from "react";
 import "./styles/Timeline.css";
 export default function Timeline() {
+    const resetTimeline = () => {
+        const a = document.querySelector(".dark");
+        const dots = document.querySelectorAll(".dot");
+        const dotcircle = document.querySelectorAll(".dotcircle");
+        const line = document.querySelector(".tlline");
+        const timelineCont = document.querySelector(".timelineCont");
+        var padding = window
+            .getComputedStyle(timelineCont)
+            .paddingLeft.substring(
+                0,
+                window.getComputedStyle(timelineCont).paddingLeft.length - 2
+            );
+        console.log(
+            `${line.offsetLeft - a.offsetLeft}px`,
+            window
+                .getComputedStyle(timelineCont)
+                .paddingLeft.substring(
+                    0,
+                    window.getComputedStyle(timelineCont).paddingLeft.length - 2
+                )
+        );
+
+        for (var i of dots) {
+            i.style.left = `${line.offsetLeft - a.offsetLeft - padding - 10}px`;
+        }
+    };
+
+    setTimeout(resetTimeline, 100);
+    window.addEventListener('resize',resetTimeline);
     return (
         <>
             <div className="timelineCont">
-                <div className="heading">
-                    Timeline
-                </div>
+                <div className="heading">Timeline</div>
+                <div className="tlline"></div>
                 <div class="tlboxwrap tlfirst">
                     <div className="tlboxes dark" id="tlbox1">
                         1st February 2022
+                        <div className="dot"></div>
+                        {/* <div className="dotcircle"></div> */}
                     </div>
                     <div className="tlboxes" id="tlbox2">
                         Registration Open
@@ -19,6 +49,8 @@ export default function Timeline() {
                 <div class="tlboxwrap">
                     <div className="tlboxes dark" id="tlbox3">
                         28th February 2022
+                        <div className="dot"></div>
+                        {/* <div className="dotcircle"></div> */}
                     </div>
                     <div className="tlboxes" id="tlbox4">
                         Registration Close
@@ -28,6 +60,8 @@ export default function Timeline() {
                 <div class="tlboxwrap">
                     <div className="tlboxes dark" id="tlbox5">
                         2nd march 2022
+                        <div className="dot"></div>
+                        {/* <div className="dotcircle"></div> */}
                     </div>
                     <div className="tlboxes" id="tlbox6">
                         Opening Ceremony
@@ -37,6 +71,8 @@ export default function Timeline() {
                 <div class="tlboxwrap">
                     <div className="tlboxes dark" id="tlbox7">
                         5th March- 10th March 2022
+                        <div className="dot"></div>
+                        {/* <div className="dotcircle"></div> */}
                     </div>
                     <div className="tlboxes" id="tlbox8">
                         Panel Mentorship
@@ -46,6 +82,8 @@ export default function Timeline() {
                 <div class="tlboxwrap">
                     <div className="tlboxes dark" id="tlbox9">
                         23rd March 2022
+                        <div className="dot"></div>
+                        {/* <div className="dotcircle"></div> */}
                     </div>
                     <div className="tlboxes" id="tlbox10">
                         Contribution deadline
